db9da1c6af2678ae888bbe5b909ff55ddb653c0f
Kesdiael Ken <112612563+Kiaelen@users.noreply.github.com>
2025-03-17 21:26:37 -0400

    updates

--- src/colorization/samplers.py
+++ src/colorization/samplers.py
@@ -68,8 +68,8 @@ def sample_stage_1(
                 # Replace component in noisy images with component from fixed image
                 im_noisy_component = torch.zeros(im_noisy.shape, device=noisy_images.device, dtype=noisy_images.dtype)
                 noisy_images_component = torch.zeros(im_noisy.shape, device=noisy_images.device, dtype=noisy_images.dtype)
-                im_noisy_component += view[0].imprint(im_noisy)
-                noisy_images_component += view[1].imprint(noisy_images[0])
+                im_noisy_component += views[0].imprint(im_noisy)
+                noisy_images_component += views[1].imprint(noisy_images[0])
                 noisy_images = im_noisy_component + noisy_images_component
 
                 noisy_images = noisy_images[None] / len(views)
