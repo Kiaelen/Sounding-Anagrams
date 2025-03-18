c3f3d8a8db790b059c9b767d7ec18b9f37b35ae0
Kesdiael Ken <112612563+Kiaelen@users.noreply.github.com>
2025-03-17 21:25:38 -0400

    updates

--- src/colorization/samplers.py
+++ src/colorization/samplers.py
@@ -68,14 +68,11 @@ def sample_stage_1(
                 # Replace component in noisy images with component from fixed image
                 im_noisy_component = torch.zeros(im_noisy.shape, device=noisy_images.device, dtype=noisy_images.dtype)
                 noisy_images_component = torch.zeros(im_noisy.shape, device=noisy_images.device, dtype=noisy_images.dtype)
-                for i, view in enumerate(views):
-                    if i % 2 == 0:
-                        im_noisy_component += view.inverse_view(im_noisy)
-                    else:
-                        noisy_images_component += view.inverse_view(noisy_images[0])
+                im_noisy_component += view[0].imprint(im_noisy)
+                noisy_images_component += view[1].imprint(noisy_images[0])
                 noisy_images = im_noisy_component + noisy_images_component
 
-                noisy_images = noisy_images[None] / 2
+                noisy_images = noisy_images[None] / len(views)
                 
                 # im_noisy_component = views[0].inverse_view(im_noisy).to(noisy_images.device).to(noisy_images.dtype)
                 # noisy_images_component = views[1].inverse_view(noisy_images[0])
