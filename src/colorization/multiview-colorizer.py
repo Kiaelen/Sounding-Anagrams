423d0ead82c3ae09fd8aee4763b7bd01eeb3973a
Kesdiael Ken <112612563+Kiaelen@users.noreply.github.com>
2025-03-18 00:40:25 -0400

    updates

--- src/colorization/multiview-colorizer.py
+++ src/colorization/multiview-colorizer.py
@@ -54,10 +54,9 @@ class FactorizedColorization(nn.Module):
         self.inverse_color = inverse_color
 
         # get views
-        # self.views = []
-        # for view in views:
-        #     self.views += [LView_Composit(view), ABView_Composit(view)]
-        self.views = [LView_Composit(view), ABView_Composit(view)]
+        self.views = []
+        for view in views:
+            self.views += [LView_Composit(view), ABView_Composit(view)]
 
         self.num_inference_steps = kwargs.get("num_inference_steps", 30)
         self.guidance_scale = kwargs.get("guidance_scale", 10.0)
